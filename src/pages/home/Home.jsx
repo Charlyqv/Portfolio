f2d86e295717a7606d25d39fe01e1a7dd49b3b54
Carlos <charlyqv98@gmail.com>
2023-07-20 16:40:03 -0600

    Avance en el Portfolio

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,31 @@
+import Profile from "../../assets/home.jpg";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home section grid'>
+      <img src={ Profile } alt='' className='home__img' />
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I'm Carlos Villaseñor Quintero</span> Web Designer
+          </h1>
+          <p className="home__description">Yo soy yo, la mera pistola, y lo que siempre
+            digo es que soy una cosa barbara
+          </p>
+          <Link to='/about' className='button'>
+            More About Me{' '}
+            <span className='button__icon'>
+              <FaArrowRight/>
+            </span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="color__block__left"></div>
+      {/* <div className="color__block__right"></div> */}
+    </section>
   )
 }
