636f7e9f37048feb0be552a0f714fb39440ade2d
Carlos Villaseñor Quintero <charlyqv98@gmail.com>
2023-07-20 21:39:34 -0600

    Avance para la pagina de 'Contact'

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,21 @@
+import {
+  FaEnvelopeOpen,
+  FaPhoneSquareAlrt,
+  FaFacebookF,
+  FaTwitter,
+  FaYoutube,
+  FaDribble,
+} from 'react-icons/fa';
 
+import { FiSend } from 'react-icons/fi';
+import "./contact.css";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='contact section'>
+      <h2 className="section__title">
+        Get In <span>Touch</span>
+      </h2>
+    </section>
   )
 }
