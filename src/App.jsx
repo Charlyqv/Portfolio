2e93fa273cf9d42859ae815f358dd0c70185cc8b
Carlos <charlyqv98@gmail.com>
2023-07-21 10:49:53 -0600

    Configuración del Tema

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { About } from './pages/about/About'
 import { Contact } from './pages/contact/Contact'
 import { Home } from './pages/home/Home'
 import { Portfolio } from './pages/portfolio/Portfolio'
+import { Themes } from './components/Themes';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+      <Themes/>
       <Routes>
         <Route index element={<Home />}/>
         <Route path='about' element={<About />}/>
---
+++ src/components/ThemeItem.jsx
@@ -0,0 +1,10 @@
+
+
+export const ThemeItem = ({ color, img, changeColor }) => {
+  return (
+    <img src={img} alt="" className="theme__img" onClick={() => {
+      changeColor(color)
+      }} 
+    />
+  )
+}
